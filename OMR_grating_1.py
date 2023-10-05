c0790c439540a3943d58b63382e1e7acfd001c81
FilosaLab <98265456+FilosaLab@users.noreply.github.com>
2023-10-05 12:24:54 +0200

    Add files via upload

---
+++ OMR_grating_1.py
@@ -0,0 +1,18 @@
+from psychopy import visual, core, event
+
+win0 = visual.Window([800, 600], screen=1, monitor= 'testMonitor', fullscr=True, color=[0,0,0], units='pix')
+
+grat_stim = visual.GratingStim(win=win0, tex='sqr', units= 'pix', pos=(0.0,0.0), size=800, sf=0.2, ori= 0.0, phase=(0.0,0.0))
+
+
+while(1):
+    grat_stim.setPhase(0.2, '+')
+    grat_stim.draw()
+    win0.flip()
+    
+    if len(event.getKeys()) > 0:
+        break
+    event.clearEvents()
+
+win0 = close()
+
